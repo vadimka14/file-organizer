1c8d512923621d47c7008d6f4117e80aa92d4702
vadimka14 <vadimgolovach2017@gmail.com>
2026-08-23 12:38:45 +0200

    map

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+	var DefaultRules = map[string]string{
+		".jpg":  "Images",
+		".jpeg": "Images",
+		".png":  "Images",
+		".pdf":  "Documents",
+		".doc":  "Documents",
+		".docx": "Documents",
+		".txt":  "Documents",
+		".mp3":  "Music",
+		".wav":  "Music",
+		".mp4":  "Video",
+		".avi":  "Video",
+		".zip":  "Archives",
+		".rar":  "Archives",
+	}
+
+	for key, value := range DefaultRules {
+		fmt.Printf("Расширение: %s -> Папка: %s\n", key, value)
+	}
 
 }
